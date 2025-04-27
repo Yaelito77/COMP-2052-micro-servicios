36d9b2192fade0df67fd94d8da81df27e5f01934
Yaelito77 <Yaelfabian007@gmail.com>
2025-04-27 18:38:17 -0400

    si

--- Modulo-03/leccion-01/app.py
+++ Modulo-03/leccion-01/app.py
@@ -1,22 +1,76 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
+from flask import Flask, render_template, request, redirect, url_for
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'clave_secreta_segura'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///usuarios.db'
+app.secret_key = 'clave_secreta'
 
-db = SQLAlchemy(app)
-login_manager = LoginManager(app)
+# Configurando Flask-Login
+login_manager = LoginManager()
+login_manager.init_app(app)
 login_manager.login_view = 'login'
 
-from modelos import Usuario
+# Usuario de ejemplo
+class User(UserMixin):
+    def __init__(self, id, username, password_hash, role):
+        self.id = id
+        self.username = username
+        self.password_hash = password_hash
+        self.role = role
 
+# Base de datos simulada
+users = {
+    "Saul": User(1, "Saul", generate_password_hash("1234"), "admin"),
+    "Janiel": User(2, "Janiel", generate_password_hash("abcd"), "user")
+}
+
+# Cargar usuario
 @login_manager.user_loader
 def load_user(user_id):
-    return Usuario.query.get(int(user_id))
+    for user in users.values():
+        if str(user.id) == user_id:
+            return user
+    return None
 
-from rutas import *
+# Ruta de inicio
+@app.route('/')
+def index():
+    return "Bienvenido a mi aplicación. <a href='/login'>Iniciar sesión</a>"
+
+# Ruta de login
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = users.get(username)
+
+        if user and check_password_hash(user.password_hash, password):
+            login_user(user)
+            return redirect(url_for('dashboard'))
+        else:
+            return "Usuario o contraseña incorrectos. <a href='/login'>Intentar de nuevo</a>"
+
+    return '''
+        <form method='post'>
+            Usuario: <input type='text' name='username'><br>
+            Contraseña: <input type='password' name='password'><br>
+            <input type='submit' value='Iniciar sesión'>
+        </form>
+    '''
+
+# Ruta protegida
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    return f"Hola, {current_user.username}! Tu rol es: {current_user.role}. <a href='/logout'>Cerrar sesión</a>"
+
+# Ruta de logout
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return "sesión cerrada. <a href='/login'>Iniciar sesión de nuevo</a>"
 
 if __name__ == '__main__':
     app.run(debug=True)
--- Modulo-03/leccion-01/forms.py
+++ Modulo-03/leccion-01/forms.py
@@ -1,17 +1,1 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Length, EqualTo
 
-class RegistroForm(FlaskForm):
-    username = StringField('Nombre de usuario', validators=[DataRequired(), Length(min=4, max=25)])
-    password = PasswordField('Contraseña', validators=[DataRequired(), Length(min=6)])
-    confirm_password = PasswordField('Confirmar contraseña', validators=[
-        DataRequired(),
-        EqualTo('password', message='Las contraseñas deben coincidir.')
-    ])
-    submit = SubmitField('Registrarse')
-
-class LoginForm(FlaskForm):
-    username = StringField('Nombre de usuario', validators=[DataRequired()])
-    password = PasswordField('Contraseña', validators=[DataRequired()])
-    submit = SubmitField('Iniciar sesión')
